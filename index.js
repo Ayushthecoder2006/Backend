f7f86b5bbc9a3c72b5cdb01b8110310d9caee498
Ayush Maurya <mauryaayush0812@gmail.com>
2026-09-03 00:56:15 +0530

    create file delete file and same for folder

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const fs = require("fs")
+
+// Create file
+// fs.writeFileSync("index.txt", "Hello")
+
+// Delete file
+fs.unlinkSync("index.txt")
+
+// Create folder
+// fs.mkdirSync("folder")
+
+// Delete folder
+fs.rmdirSync("folder")
