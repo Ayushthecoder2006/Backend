f488b590f06c41c8957e76a875bd033c4e075445
Ayush Maurya <mauryaayush0812@gmail.com>
2026-09-03 18:52:06 +0530

    creating server through node.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-// Create file
-// fs.writeFileSync("index.txt", "Hello")
+// // Create file
+// // fs.writeFileSync("index.txt", "Hello")
 
-// Delete file
-fs.unlinkSync("index.txt")
+// // Delete file
+// fs.unlinkSync("index.txt")
 
-// Create folder
-// fs.mkdirSync("folder")
+// // Create folder
+// // fs.mkdirSync("folder")
 
-// Delete folder
-fs.rmdirSync("folder")
+// // Delete folder
+// fs.rmdirSync("folder")
+
+
+// let data = fs.readFileSync("index.txt")
+// console.log(data.toString(),"hehehehehe");
+
+// oprating System
+// let os = require('os')
+// total rem
+// console.log(os.totalmem()/1024/1024/1024);
+// console.log(os.freemem()/1024/1024/1024);
+// console.log(os.cpus());
+// console.log(os.arch());
+// console.log(os.uptime()/3600);
+
+
+let http = require('http')
+let server = http.createServer((req,res)=>{
+    res.write("Hello ")
+    res.end("Hello")
+})
+server.listen(3000,()=>{
+    console.log("server running......")
+})
+
+
+
